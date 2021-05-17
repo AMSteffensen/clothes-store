44dbcb45b20189297dfaeeaae656fa5ad55a3551
steand <andreas.steffensen@n5n.no>
2021-05-17 23:49:16 +0200

    finalized shopping cart

---
+++ src/components/shopping/CartItem.tsx
@@ -0,0 +1,41 @@
+import Button from "@material-ui/core/Button";
+import { CartItemType } from "../../App";
+
+import { Wrapper } from "./CartItem.styles";
+
+type Props = {
+  item: CartItemType;
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
+  <Wrapper>
+    <div>
+      <h3>{item.title}</h3>
+      <div className="information">
+        <p>Price: ${item.price}</p>
+        <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+      </div>
+      <Button
+        className="buttons"
+        size="small"
+        variant="contained"
+        onClick={() => removeFromCart(item.id)}
+      >
+        -
+      </Button>
+      <p>{item.amount}</p>
+      <Button
+        className="buttons"
+        size="small"
+        variant="contained"
+        onClick={() => addToCart(item)}
+      >
+        +
+      </Button>
+    </div>
+    <img src={item.image} alt={item.title} />
+  </Wrapper>
+);
+export default CartItem;
---
+++ src/components/shopping/cart.styles.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.aside`
+  font-family: 500px;
+  padding: 200px;
+`;
